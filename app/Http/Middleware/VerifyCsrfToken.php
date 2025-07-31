9d11256e0db15460e58d901fa1899f17d46dd6c8
roinflames <rod.reyes.s@gmail.com>
2025-07-31 13:55:11 -0400

    Agrego VerifyCsrfToken por error en despliegue

---
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Illuminate\Foundation\Http\Middleware\VerifyCsrfToken as Middleware;
+
+class VerifyCsrfToken extends Middleware
+{
+    /**
+     * Las URIs que deben excluirse de la verificación CSRF.
+     *
+     * @var array<int, string>
+     */
+    protected $except = [
+        'register',
+        'login',
+        // agrega más rutas si es necesario
+    ];
+}
